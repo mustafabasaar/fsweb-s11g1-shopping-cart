acd930073af017acbb2e0ed471afb6bb3ec425d4
mustafabasar96 <mustafabasar96@hotmail.com>
2023-10-30 19:49:41 +0300

    localstora ge eklencek

--- src/App.js
+++ src/App.js
@@ -7,29 +7,49 @@ import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 function App() {
   const [products, setProducts] = useState(data);
   const [cart, setCart] = useState([]);
 
   const addItem = (item) => {
-    // verilen itemi sepete ekleyin
+    console.log("ürünleri ver", products);
+    setCart([...cart, item]);
   };
+  function removeItem(item) {
+    console.log("ver babaa", item);
+    const index = cart.indexOf(item);
+
+    if (index !== -1) {
+      cart.splice(index, 1);
+
+      console.log(`${item} sepetten kaldırıldı.`);
+    }
+    setCart([...cart]);
+  }
 
   return (
-    <div className="App">
-      <Navigation cart={cart} />
+    <ProductContext.Provider
+      value={{ setProducts, products, addItem, removeItem }}
+    >
+      <CartContext.Provider value={{ cart, setCart }}>
+        <div className="App">
+          <Navigation />
 
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+          {/* Routelar */}
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
-    </div>
+            <Route path="/cart">
+              <ShoppingCart />
+            </Route>
+          </main>
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
   );
 }
 
